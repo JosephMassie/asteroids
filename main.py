7cfb2499992133ef7f0b8ab45d2c022881a6a5d5
Joseph Massie <josephmmassie@gmail.com>
2024-10-14 14:16:16 -0700

    Created a simple base CircleShape class to be the base of all game objects
    Created Player, Shot, Asteroid, and AsteroidField classes as children of CircleShape
      to represent the games core objects
    
    Player:
      contains all of the logic to track player input and spawn Shots when appropriate
    
    Shot:
      simple object to represent bullets the player shoots, only has base methods to draw
      and update
    
    Asteroid:
      simple object to represent asteroids in the game world, only has base methods to
      draw and update
    
    AsteroidField:
      an object created to manage all Asteroids including spawning new ones just of the
      edge of the map. creates new asteroids of three possible sizes and gives them a
      random velocity coming in from one of the edges of the screen
    
    Updated the game loop with sprite groups to handle updating and drawing all of these
      objects in a scalable fashion

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 import pygame
 from constants import *
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    drawables = pygame.sprite.Group()
+    updateables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (drawables, updateables)
+    Shot.containers = (shots, drawables, updateables)
+    Asteroid.containers = (asteroids, drawables, updateables)
+    AsteroidField.containers = (updateables)
+
+    AsteroidField()
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((50, 50, 50))
+
+        for obj in updateables:
+            obj.update(dt)
+        
+        for ast in asteroids:
+            for bullet in shots:
+                hit = pygame.sprite.collide_circle(ast, bullet)
+                if hit:
+                    print(f"hit @ {bullet.position}")
+                    ast.kill()
+                    bullet.kill()
+
+        screen.fill((20, 20, 20))
+        for obj in drawables:
+            obj.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(MAX_FPS) / 1000
 
 if __name__ == "__main__":
     main()
